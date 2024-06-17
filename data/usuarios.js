f850a67ce7436ea90ed40efc5a4a4d5972f7c2cd
Fabio D. Argañaraz A <ing.fabio.arg@gmail.com>
2024-06-17 14:34:45 -0300

    Create usuarios.js

---
+++ data/usuarios.js
@@ -0,0 +1,7 @@
+module.exports = [
+  { id: 1, username: 'alice', password: 'alice123' },
+  { id: 2, username: 'bob', password: 'bob123' },
+  { id: 3, username: 'carol', password: 'carol123' },
+  { id: 4, username: 'dave', password: 'dave123' },
+  { id: 5, username: 'eve', password: 'eve123' }
+];
