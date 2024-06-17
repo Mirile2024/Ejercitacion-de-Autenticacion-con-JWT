b91333a3872e5aeafa1c3dd144484d70db7761b9
Fabio D. Argañaraz A <ing.fabio.arg@gmail.com>
2024-06-17 14:35:34 -0300

    Create tareas.js

---
+++ data/tareas.js
@@ -0,0 +1,6 @@
+module.exports = [
+  { id: 1, userId: 1, tarea: 'Comprar pan' },
+  { id: 2, userId: 1, tarea: 'Leer un libro' },
+  { id: 3, userId: 2, tarea: 'Ir al gimnasio' },
+  { id: 4, userId: 3, tarea: 'Llamar a mamá' }
+];
